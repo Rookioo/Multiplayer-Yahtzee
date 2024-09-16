bd721449209f0221312987c5f2ffd6396ba2ea98
Rookioo <andersond5235@my.uwstout.edu>
2024-09-15 22:03:54 -0500

    I've done a lot of troubleshooting with VS code as a whole with java not working.
    Created Main Class and the start of default player prompt to set up the game

--- main.java
+++ main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args) {
-        System.out.print("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        //Boolean running = true;
+        int numPlayers;
+        ArrayList<String> words = new ArrayList<String>();
+
+        // Code to be used for timings
+        //long then = System.currentTimeMillis();
+        //long now = System.currentTimeMillis();
+        //long elapsedTime = System.currentTimeMillis() - startTime;
+        //long elapsedSeconds = elapsedTime / 1000;
+        //long secondsDisplay = elapsedSeconds % 60;
+        //long elapsedMinutes = elapsedSeconds / 60;
+        System.out.println("\n\nHi Welcome To Yahtzee!\n");
+        System.out.println("Please type number of players (Accepted inputs: 1-6)");
+        numPlayers = scanner.nextInt();
+        if (numPlayers > 1) {
+            System.out.println("You are playing as " + numPlayers + " Players\n");
+        } else {
+            System.out.println("You are playing as " + numPlayers + " Player\n");
+        }
+
+
+        System.out.println("Please roll for playing order (Accepted inputs: roll)");
+
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println("Player one roll");
+        }
+
+
+        scanner.close();
+
     }
 }
