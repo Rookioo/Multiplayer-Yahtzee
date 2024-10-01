3e9166a85987c42e8aaff5c8fd075c54719cbacb
Rookioo <andersond5235@my.uwstout.edu>
2024-09-30 23:12:39 -0500

    Final Commit with documentation

--- Dice.java
+++ Dice.java
@@ -7,11 +7,19 @@
  */
 
 public class Dice {
-    // Method to roll a dice and return the value
+    /**
+     * Method Description: Method to simulate a dice roll
+     * @return int returns the integer 1-6 at random
+     */
     public static int roll() {
         return (int) (Math.random() * 6 + 1); // Simulate dice roll
     }
 
+    /**
+     * Method Description: Rolls multiple dice at once for convinience sake
+     * @paramater numDice int for the number of dice to be rolled
+     * @return rolls array of integers of the different roll values
+     */
     public static int[] rollMultiple(int numDice) {
         int[] rolls = new int[numDice];
         for (int i = 0; i < numDice; i++) {
